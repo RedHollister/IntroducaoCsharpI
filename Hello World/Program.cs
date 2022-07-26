fbfabd27e0febb0566a014b213b3d2e466fa42d3
Elton Guilherme <elton.guilherme@hotmail.com>
2022-07-25 22:55:52 -0300

    Aula 2 Continuação

---
+++ Hello World/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+ //Pasta lógica
+ namespace HelloWorld
+{
+    class Program
+    {
+        //Isso é um comentário de uma linha! Tudo que for escrito será ignorado
+        //Pelo compilador
+            
+            /* Este comentário 
+             * é
+             * um de
+             * mais uma linha
+             */
+            
+        //Método Main:
+        //Ponto de entrada para a aplicação, ou seja, é
+        //O que define o início da aplicação
+        static void Main(string[] args)
+        {
+            //Imprimir na tela o famoso "Olá, Mundo!"
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Meu nome é Elton!");
+
+        }
+    }
+}
+
